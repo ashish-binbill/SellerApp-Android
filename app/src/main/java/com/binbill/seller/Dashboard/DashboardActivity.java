d44e702e3de3fface4eb2d702c95b5d41470a1e9
shruti vig <shruti.vig@payu.in>
2018-09-28 12:54:17 +0530

    Increased negative margin for shop image in hamburger

--- app/src/main/java/com/binbill/seller/Dashboard/DashboardActivity.java
+++ app/src/main/java/com/binbill/seller/Dashboard/DashboardActivity.java
@@ -318,7 +318,7 @@ public class DashboardActivity extends BaseActivity implements YesNoDialogFragme
                 RelativeLayout.LayoutParams.MATCH_PARENT
         );
 
-        int integer15 = Utility.convertDPtoPx(this, 15);
+        int integer15 = Utility.convertDPtoPx(this, 17);
         params.setMargins(-integer15, -integer15, -integer15, -integer15);
         iv_user_image.setLayoutParams(params);
 
